55444c70fb1895c04bca6b72750cfb4960809089
ElonDusk26 <elondusk26@gmail.com>
2022-10-23 18:00:45 +0200

    comments

--- client.cpp
+++ client.cpp
@@ -9,8 +9,8 @@ char getch(bool block = false) {
         struct termios old = {0};
         if (tcgetattr(0, &old) < 0)
                 perror("tcsetattr()");
-        old.c_lflag &= ~ICANON;
-        old.c_lflag &= ~ECHO;
+        old.c_lflag &= ~ICANON; //devilish bitwise operations: & bitwise AND (removes bits)
+        old.c_lflag &= ~ECHO; // ~ bitwise NOT operator (inverts like ! but bitwise). translated to "keep all but ECHO flag"
         old.c_cc[VMIN] = 1;
         old.c_cc[VTIME] = 0;
         if (tcsetattr(0, TCSANOW, &old) < 0)
@@ -24,8 +24,8 @@ char getch(bool block = false) {
 
         if (read(0, &buf, 1) < 0)
                 //perror ("read()");
-        old.c_lflag |= ICANON;
-        old.c_lflag |= ECHO;
+        old.c_lflag |= ICANON; //devilish bitwise operations: | bitwise OR (adds bits)
+        old.c_lflag |= ECHO; //translated "add ECHO"
         if (tcsetattr(0, TCSADRAIN, &old) < 0)
                 perror ("tcsetattr ~ICANON");
         return (buf);
